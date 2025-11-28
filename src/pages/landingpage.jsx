7841d09bd7d7f41fb10cbcd53becdb03b75ba72b
Danujainoshika <danujamanukularathna@gmail.com>
2025-11-28 16:59:42 +0530

    add BenefitSection component to LandingPage

--- src/pages/landingpage.jsx
+++ src/pages/landingpage.jsx
@@ -1,9 +1,11 @@
+import { BenefitSection } from "../components/benifitsection";
 import { HeroSection } from "../components/herosection";
 
 export function LandingPage() {
     return (
-        <div  className="w-full min-h-[calc(100%-100px)] bg-primary flex flex-col justify-center items-center overflow-y-scroll px-[50px]">
+        <div  className="w-full min-h-[calc(100%-100px)] bg-primary flex flex-col justify-center items-center overflow-y-scroll ">
             <HeroSection/>
+            <BenefitSection/>
         </div>
     )
 }
