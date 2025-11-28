d93494f78f661b5feab22d4b022fb3488667cdd2
Danujainoshika <danujamanukularathna@gmail.com>
2025-11-28 16:42:03 +0530

    enhance HeroSection layout and content; update LandingPage styling

--- src/pages/landingpage.jsx
+++ src/pages/landingpage.jsx
@@ -2,7 +2,7 @@ import { HeroSection } from "../components/herosection";
 
 export function LandingPage() {
     return (
-        <div  className="w-full min-h-[calc(100%-100px)] bg-primary  ">
+        <div  className="w-full min-h-[calc(100%-100px)] bg-primary flex flex-col justify-center items-center overflow-y-scroll px-[50px]">
             <HeroSection/>
         </div>
     )
